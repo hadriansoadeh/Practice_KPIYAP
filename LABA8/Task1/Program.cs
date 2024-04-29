b3e21b83f267ce809bd15c2ee9e4f282cc143e7d
hadriansoadeh <167083995+hadriansoadeh@users.noreply.github.com>
2024-04-29 12:52:41 +0300

    готовая тема 8

---
+++ LABA8/Task1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+struct STUDENT
+{
+    public string LastNameAndInitials;
+    public int GroupNumber;
+    public int[] Performance;
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите кол-во студентов");
+        int NumStudents = int.Parse(Console.ReadLine());
+        STUDENT[] students = new STUDENT[NumStudents];
+
+        
+        for (int i = 0; i < NumStudents; i++)
+        {
+            Console.WriteLine($"Введите данные студента {i + 1}:");
+
+           
+            Console.Write("Фамилия и инициалы: ");
+            students[i].LastNameAndInitials = Console.ReadLine();
+
+            
+            Console.Write("Номер группы: ");
+            students[i].GroupNumber = int.Parse(Console.ReadLine());
+
+            
+            students[i].Performance = new int[5];
+            for (int j = 0; j < 5; j++)
+            {
+                Console.Write($"Введите оценку {j + 1}: ");
+                students[i].Performance[j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+       
+        Array.Sort(students, (s1, s2) =>
+        {
+            double avg1 = s1.Performance.Average();
+            double avg2 = s2.Performance.Average();
+            return avg1.CompareTo(avg2);
+        });
+
+        
+        bool found = false;
+        foreach (var student in students)
+        {
+            double avgGrade = student.Performance.Average();
+            if (avgGrade >= 4)
+            {
+                Console.WriteLine($"Студент: {student.LastNameAndInitials}, Группа: {student.GroupNumber}");
+                found = true;
+            }
+        }
+
+        if (!found)
+        {
+            Console.WriteLine("Нет студентов с оценками 4 и 5.");
+        }
+    }
+}
---
+++ LABA8/Task2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+enum EmployeePosition
+{
+    Manager, 
+    Assistant, 
+    Developer, 
+    Trainee 
+}
+
+class Accountant
+{
+    
+    public bool AskForBonus(EmployeePosition position, int hoursWorked)
+    {
+        Console.WriteLine("Введите пороговое значение отработанных часов для премии");
+        int BonusThresholdHours = int.Parse(Console.ReadLine()); 
+
+        
+        if (hoursWorked > BonusThresholdHours)
+        {
+            Console.WriteLine($"Сотруднику с должностью {position} положена премия!");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine($"Сотруднику с должностью {position} премия не положена.");
+            return false;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+        Accountant accountant = new Accountant();
+
+        
+        bool bonusGranted = accountant.AskForBonus(EmployeePosition.Assistant, 170);
+        
+
+
+        if (bonusGranted)
+        {
+            Console.WriteLine("Премия была начислена.");
+        }
+        else
+        {
+            Console.WriteLine("Премия не была начислена.");
+        }
+    }
+}
