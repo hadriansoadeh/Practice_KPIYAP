4f548333e085299bb7cad0d201daa8d8fb120f52
hadriansoadeh <167083995+hadriansoadeh@users.noreply.github.com>
2024-05-02 14:39:34 +0300

    Готовая тема 10

---
+++ LABA10/Task1/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Task1
+{
+    class Notebook
+    {
+        private string title;
+        private int numPages;
+
+        public Notebook(string title, int numPages)
+        {
+            this.title = title;
+            this.numPages = numPages;
+        }
+
+        public void SetTitle(string title)
+        {
+            this.title = title;
+        }
+
+        public string GetTitle()
+        {
+            return title;
+        }
+
+        public void SetNumPages(int numPages)
+        {
+            this.numPages = numPages;
+        }
+
+        public int GetNumPages()
+        {
+            return numPages;
+        }
+
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine("Notebook Title: " + title);
+            Console.WriteLine("Number of Pages: " + numPages);
+        }
+
+        public virtual void Cost()
+        {
+            Console.WriteLine("Cost: 15");
+        }
+    }
+
+    class SpecialNotebook : Notebook
+    {
+        private string specialFeature;
+
+        public SpecialNotebook(string title, int numPages, string specialFeature) : base(title, numPages)
+        {
+            this.specialFeature = specialFeature;
+        }
+
+        public override void DisplayInfo()
+        {
+            base.DisplayInfo();
+            Console.WriteLine("Special Feature: " + specialFeature);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Notebook notebook1 = new Notebook("Simple Notebook", 100);
+            notebook1.DisplayInfo();
+            notebook1.Cost();
+
+            Console.WriteLine();
+
+            SpecialNotebook specialNotebook1 = new SpecialNotebook("Special Notebook", 120, "Waterproof");
+            specialNotebook1.DisplayInfo();
+            specialNotebook1.Cost();
+        }
+    }
+}
---
+++ LABA10/Task2/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Task2
+{
+    class A
+    {
+        protected int a = 10;
+        protected int b = 5;
+
+        public int GetC()
+        {
+            return a + b;
+        }
+    }
+
+    class B : A
+    {
+        private int d = 3;
+
+        public int GetC2()
+        {
+            int result = 0;
+
+            switch (GetC())
+            {
+                case 15:
+                    result = a * b * d;
+                    break;
+                case 20:
+                    result = (a + b) * d;
+                    break;
+                default:
+                    result = a + b + d;
+                    break;
+            }
+
+            return result;
+        }
+
+        public B() : base() { }
+
+        public B(int a, int b, int d)
+        {
+            this.a = a;
+            this.b = b;
+            this.d = d;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            A objA = new A();
+            Console.WriteLine("Value of property C in class A: " + objA.GetC());
+
+            B objB1 = new B();
+            Console.WriteLine("Value of property C2 in class B (created with base constructor): " + objB1.GetC2());
+
+            B objB2 = new B(7, 3, 2);
+            Console.WriteLine("Value of property C2 in class B (created with custom constructor): " + objB2.GetC2());
+        }
+    }
+
+}
