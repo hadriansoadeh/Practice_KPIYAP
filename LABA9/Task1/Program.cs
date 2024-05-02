7271660884e365abe5d91628237ba40bb29cbdc0
hadriansoadeh <167083995+hadriansoadeh@users.noreply.github.com>
2024-05-02 14:38:28 +0300

    Готовая тема 9

---
+++ LABA9/Task1/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Task1
+{
+    public interface Iy
+    {
+        double F0(double w);
+        double F1(double w);
+    }
+
+    public interface Iz
+    {
+        double F0(double w);
+        double F1(double w);
+    }
+
+    class Program : Iy, Iz
+    {
+        public double F0(double w)
+        {
+            return Math.Cos(w);
+        }
+
+        public double F1(double w)
+        {
+            return Math.Exp(w);
+        }
+
+        public double IyF0(double w)
+        {
+            return ((Iy)this).F0(w);
+        }
+
+        public double IyF1(double w)
+        {
+            return ((Iy)this).F1(w);
+        }
+
+        public double IzF0(double w)
+        {
+            return ((Iz)this).F0(w);
+        }
+
+        public double IzF1(double w)
+        {
+            return ((Iz)this).F1(w);
+        }
+
+        static void Main()
+        {
+            Program program = new Program();
+            double w = 1.0;
+
+            Console.WriteLine("F0 from Iy: " + program.IyF0(w));
+            Console.WriteLine("F1 from Iy: " + program.IyF1(w));
+            Console.WriteLine("F0 from Iz: " + program.IzF0(w));
+            Console.WriteLine("F1 from Iz: " + program.IzF1(w));
+        }
+    }
+}
+    
---
+++ LABA9/Task2/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace Task2
+{
+    using System;
+
+    class FixedLengthStringArray
+    {
+        private string[] array;
+        private int lowerBound;
+        private int upperBound;
+
+        public FixedLengthStringArray(int lowerBound, int upperBound)
+        {
+            this.lowerBound = lowerBound;
+            this.upperBound = upperBound;
+            array = new string[upperBound - lowerBound + 1];
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index < lowerBound || index > upperBound)
+                {
+                    throw new IndexOutOfRangeException("Index is out of range");
+                }
+                return array[index - lowerBound];
+            }
+            set
+            {
+                if (index < lowerBound || index > upperBound)
+                {
+                    throw new IndexOutOfRangeException("Index is out of range");
+                }
+                array[index - lowerBound] = value;
+            }
+        }
+
+        public FixedLengthStringArray Concatenate(FixedLengthStringArray otherArray)
+        {
+            FixedLengthStringArray resultArray = new FixedLengthStringArray(lowerBound, upperBound + otherArray.upperBound - otherArray.lowerBound + 1);
+            for (int i = lowerBound; i <= upperBound; i++)
+            {
+                resultArray[i] = this[i];
+            }
+            for (int i = otherArray.lowerBound; i <= otherArray.upperBound; i++)
+            {
+                resultArray[i + upperBound - lowerBound + 1] = otherArray[i];
+            }
+            return resultArray;
+        }
+
+        public void PrintElement(int index)
+        {
+            Console.WriteLine("Element at index " + index + ": " + this[index]);
+        }
+
+        public void PrintArray()
+        {
+            for (int i = lowerBound; i <= upperBound; i++)
+            {
+                Console.WriteLine("Element at index " + i + ": " + this[i]);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            FixedLengthStringArray array1 = new FixedLengthStringArray(1, 3);
+            array1[1] = "Hello";
+            array1[2] = "World";
+            array1[3] = "!";
+
+            FixedLengthStringArray array2 = new FixedLengthStringArray(1, 2);
+            array2[1] = "Welcome";
+            array2[2] = "to";
+
+            FixedLengthStringArray concatenatedArray = array1.Concatenate(array2);
+
+            concatenatedArray.PrintArray();
+            concatenatedArray.PrintElement(2);
+        }
+    }
+
+}
