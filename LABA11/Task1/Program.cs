ac58227023d3925dc1e8c0a5f1b020b655f46b2c
hadriansoadeh <167083995+hadriansoadeh@users.noreply.github.com>
2024-05-02 14:39:52 +0300

    готовая тема 11

---
+++ LABA11/Task1/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Task1
+{
+    class Building
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public virtual void CalculateFoundationHeight()
+        {
+            Console.WriteLine("Расчет высоты фундамента для здания: " + name);
+        }
+    }
+
+    class Office : Building
+    {
+        public int numberOfFloors;
+
+        public override void CalculateFoundationHeight()
+        {
+            double foundationHeight = 0.05 * numberOfFloors;
+            Console.WriteLine("Высота фундамента для офисного здания " + Name + " с " + numberOfFloors + " этажами: " + foundationHeight + " метров");
+        }
+    }
+
+    class Factory : Building
+    {
+        public double weight;
+
+        public override void CalculateFoundationHeight()
+        {
+            double foundationHeight = 0.000002 * weight;
+            Console.WriteLine("Высота фундамента для здания завода " + Name + " с весом " + weight + " кг: " + foundationHeight + " метров");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Building[] buildings = new Building[5];
+
+            buildings[0] = new Building();
+            buildings[0].Name = "Здание 1";
+
+            buildings[1] = new Office();
+            buildings[1].Name = "Офисное здание 1";
+            ((Office)buildings[1]).numberOfFloors = 5;
+
+            buildings[2] = new Factory();
+            buildings[2].Name = "Завод 1";
+            ((Factory)buildings[2]).weight = 50000;
+
+            buildings[3] = new Building();
+            buildings[3].Name = "Здание 2";
+
+            buildings[4] = new Office();
+            buildings[4].Name = "Офисное здание 2";
+            ((Office)buildings[4]).numberOfFloors = 3;
+
+            foreach (var building in buildings)
+            {
+                building.CalculateFoundationHeight();
+            }
+        }
+    }
+
+}
---
+++ LABA11/Task2/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace Task2
+{
+    abstract class Building
+    {
+        protected string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public abstract void CalculateFoundationHeight();
+    }
+
+    class Office : Building
+    {
+        public int numberOfFloors;
+
+        public override void CalculateFoundationHeight()
+        {
+            double foundationHeight = 0.05 * numberOfFloors;
+            Console.WriteLine("Высота фундамента для офисного здания " + Name + " с " + numberOfFloors + " этажами: " + foundationHeight + " метров");
+        }
+    }
+
+    class Factory : Building
+    {
+        public double weight;
+
+        public override void CalculateFoundationHeight()
+        {
+            double foundationHeight = 0.000002 * weight;
+            Console.WriteLine("Высота фундамента для здания завода " + Name + " с весом " + weight + " кг: " + foundationHeight + " метров");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Building[] buildings = new Building[5];
+
+            buildings[0] = new Office { Name = "Офисное здание 1", numberOfFloors = 5 };
+            buildings[1] = new Factory { Name = "Завод 1", weight = 50000 };
+            buildings[2] = new Office { Name = "Офисное здание 2", numberOfFloors = 3 };
+            buildings[3] = new Office { Name = "Офисное здание 3", numberOfFloors = 8 };
+            buildings[4] = new Factory { Name = "Завод 2", weight = 100000 };
+
+            double maxFoundationHeight = 0;
+
+            foreach (var building in buildings)
+            {
+                building.CalculateFoundationHeight();
+
+                
+                if (building is Office office && office.numberOfFloors > 0)
+                {
+                    double foundationHeight = 0.05 * office.numberOfFloors;
+                    if (foundationHeight > maxFoundationHeight)
+                    {
+                        maxFoundationHeight = foundationHeight;
+                    }
+                }
+                else if (building is Factory factory && factory.weight > 0)
+                {
+                    double foundationHeight = 0.000002 * factory.weight;
+                    if (foundationHeight > maxFoundationHeight)
+                    {
+                        maxFoundationHeight = foundationHeight;
+                    }
+                }
+            }
+
+            Console.WriteLine("Максимальная высота фундамента всех зданий: " + maxFoundationHeight + " метров");
+        }
+    }
+
+}
