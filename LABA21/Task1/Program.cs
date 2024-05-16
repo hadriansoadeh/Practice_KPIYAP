7d42c26a0bd7ee1d03fbf8843be3fdb8d53a686b
hadriansoadeh <167083995+hadriansoadeh@users.noreply.github.com>
2024-05-16 11:46:43 +0300

    готовая тема 21

---
+++ LABA21/Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Task1
+{
+    public sealed class Singleton
+    {
+        private static Singleton instance = null;
+        private static readonly object padlock = new object();
+
+        Singleton()
+        {
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Singleton();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleton = Singleton.Instance;
+            singleton.DisplayMessage("Привет, мир!");
+        }
+    }
+
+}
